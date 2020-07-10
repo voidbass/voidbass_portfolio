fea0124eb1addb21d7a6a0e595d43511a328c920
Khôi Huỳnh <khoi.dn95@gmail.com>
2020-07-10 13:58:07 +0700

    Updates

---
+++ precache-manifest.de8524a4d7306d600d71c9916ccb54e0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392f7b93011bfdc75fc010fa3dd4fd8a",
+    "url": "/voidbass_portfolio/index.html"
+  },
+  {
+    "revision": "771de6dbcb6f1a994a63",
+    "url": "/voidbass_portfolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "2023bbd8cb8348f4e6ff",
+    "url": "/voidbass_portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "ca974ccf6a6baa5709d5",
+    "url": "/voidbass_portfolio/static/css/main.f11cb841.chunk.css"
+  },
+  {
+    "revision": "9219b0da449a6bcbf113",
+    "url": "/voidbass_portfolio/static/js/2.3b414dcf.chunk.js"
+  },
+  {
+    "revision": "771de6dbcb6f1a994a63",
+    "url": "/voidbass_portfolio/static/js/3.85c1608e.chunk.js"
+  },
+  {
+    "revision": "2023bbd8cb8348f4e6ff",
+    "url": "/voidbass_portfolio/static/js/4.5af5c0ec.chunk.js"
+  },
+  {
+    "revision": "ca974ccf6a6baa5709d5",
+    "url": "/voidbass_portfolio/static/js/main.1e50ca99.chunk.js"
+  },
+  {
+    "revision": "ac86d2351848219fbac8",
+    "url": "/voidbass_portfolio/static/js/runtime-main.996b99d5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/voidbass_portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/voidbass_portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/voidbass_portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/voidbass_portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/voidbass_portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/voidbass_portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "dd7053afd7a4e2fe8910b13b0ae2646a",
+    "url": "/voidbass_portfolio/static/media/logo-bitis.dd7053af.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/voidbass_portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/voidbass_portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/voidbass_portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/voidbass_portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/voidbass_portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "7f134c6a8f73a60e5b5e87adff45eb5a",
+    "url": "/voidbass_portfolio/static/media/sunasterisk-logo.7f134c6a.jpg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/voidbass_portfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
