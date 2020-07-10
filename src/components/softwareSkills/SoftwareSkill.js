9f5861afa697c4541c148a63442cc0b46508ae56
Khôi Huỳnh <khoi.dn95@gmail.com>
2020-07-10 13:54:50 +0700

    first cm

---
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./SoftwareSkill.css";
+import { skillsSection } from "../../portfolio";
+
+export default function SoftwareSkill() {
+  return (
+    <div>
+      <div className="software-skills-main-div">
+        <ul className="dev-icons">
+          {skillsSection.softwareSkills.map((skills) => {
+            return (
+              <li
+                className={
+                  skills.className ? skills.className : "software-skill-inline"
+                }
+                name={skills.skillName}
+              >
+                <i className={skills.fontAwesomeClassname}></i>
+                <p>{skills.skillName}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
